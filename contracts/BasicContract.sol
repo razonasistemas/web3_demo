2e7c071199f3b6d053adf8716811a0b39787a3c4
Luis Pando <luis@luispando.com>
2021-01-04 00:10:16 +0100

    changed names in methods of the smart contract

--- contracts/BasicContract.sol
+++ contracts/BasicContract.sol
@@ -5,12 +5,12 @@ contract BasicContract {
    string name;
    uint age;
    
-   function setInstructor(string memory _name, uint _age) public {
+   function setData(string memory _name, uint _age) public {
        name = _name;
        age = _age;
    }
    
-   function getInstructor() public view returns (string memory, uint) {
+   function getData() public view returns (string memory, uint) {
        return (name, age);
    }
 }
