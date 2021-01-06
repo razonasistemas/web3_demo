d4ef9817bd3c85472eb33ccbc2f05305dd67f5b1
Luis Pando <luis@luispando.com>
2021-01-06 19:16:22 +0100

    added bootstrap and events

--- contracts/BasicContract.sol
+++ contracts/BasicContract.sol
@@ -1,16 +1,18 @@
 pragma solidity ^0.5.16;
 
 contract BasicContract {
-    
-   string name;
-   uint age;
-   
-   function setData(string memory _name, uint _age) public {
-       name = _name;
-       age = _age;
-   }
-   
-   function getData() public view returns (string memory, uint) {
-       return (name, age);
-   }
+    string name;
+    uint256 age;
+
+    event Set(address indexed _from, uint256 _value);
+
+    function setData(string memory _name, uint256 _age) public {
+        name = _name;
+        age = _age;
+        emit Set(msg.sender, age);
+    }
+
+    function getData() public view returns (string memory, uint256) {
+        return (name, age);
+    }
 }
